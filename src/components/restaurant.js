ee1e0de6eb04bebc11d8068eedc69c603559acb9
n.konohova <natasha.konohovaa@yandex.ru>
2021-02-14 22:01:46 +0300

    HT1 (add memo)

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Menu from './menu';
 import Rate from './rate';
 import Reviews from './reviews';
 
 const Restaurant = ({restaurant}) => {
   const {menu, reviews} = restaurant;
+  const averageRating = useMemo(() => ((reviews.map(({rating}) => rating).reduce((current, next) => current + next, 0))/reviews.length), [reviews]);
   return (
     <>
       <Menu menu={menu} />
-      <Rate rating={((reviews.map(({rating}) => rating).reduce((current, next) => current + next, 0))/reviews.length)} />
+      <Rate rating={averageRating} />
       <Reviews reviews={reviews} />
     </>
   );
