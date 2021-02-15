40cc5038abda72ae8a29857b44daac78d76a4393
Alexandr <alex.develop777@gmail.com>
2021-02-15 21:16:40 +0200

    HT1

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Product from './product';
+import AverageRate from './averageRate'
 
 export default function Menu(props) {
+  let reviews = props.reviews;
+
   return (
     <div>
+      <AverageRate reviews={reviews}/>
       {props.menu.map((product) => (
         <Product key={product.id} product={product} />
       ))}
