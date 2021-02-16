c1838da9b8ee8f06fc8dacd10c57b02c3528e058
Anton <anton.zinin@decathlon.com>
2021-02-16 17:26:22 +0300

    fix error

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Menu from './menu';
 import Reviews from './reviews';
+import Rate from './rate';
+
 import style from './restaurant.module.css';
 
 export default function Restaurant(props) {
@@ -10,13 +12,16 @@ export default function Restaurant(props) {
       (sum, el) => sum + el.rating,
       0
     );
-    ratingMean = ratingSum / 2;
+    ratingMean = ratingSum / props.activeRestaurant.reviews.length;
+    ratingMean = ratingMean % 1 === 0 ? ratingMean : ratingMean.toFixed(2);
   }
   console.log(ratingMean);
   return (
     <div>
       <Menu menu={props.activeRestaurant.menu} />
-      <div className={style.ratingMean}>Средний рейтинг: {ratingMean}</div>
+      <div className={style.ratingMean}>
+        Средний рейтинг: <Rate rating={ratingMean} />
+      </div>
       <Reviews reviews={props.activeRestaurant.reviews} />
     </div>
   );
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -3,12 +3,15 @@ import Rate from './rate';
 import style from './reviews.module.css';
 
 export default function Restaurant(props) {
-  const genetateReviewJsx = (review) => (
-    <div className={style.reviewWrap}>
-      <div className={style.name}>{review.user}</div>
-      <div>{review.text}</div>
-      <Rate rating={review.rating} />
+  return (
+    <div>
+      {props.reviews.map((review) => (
+        <div key={review.user} className={style.reviewWrap}>
+          <div className={style.name}>{review.user}</div>
+          <div>{review.text}</div>
+          <Rate rating={review.rating} />
+        </div>
+      ))}
     </div>
   );
-  return <div>{props.reviews.map((review) => genetateReviewJsx(review))}</div>;
 }
