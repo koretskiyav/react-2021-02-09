10ec67fd55a67ddaeadaaa6a86ac38022763697b
elveret <lena.veret@gmail.com>
2021-02-16 11:31:42 +0300

    addition to HT1 - добавлен средний рейтинг

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Menu from './menu';
 import Reviews from './reviews';
+import Rate from './rate';
 
 export default function Restaurant(props) {
   return (
     <div>
       <Menu menu={props.item.menu} />
+      <Rate reviews={props.item.reviews} />
       <Reviews reviews={props.item.reviews} />
     </div>
   );
