6ae5c0419a5b26ee1f39860ca0265516a3e6927f
Sergey Kovalchuk <admin@vitam.in.ua>
2021-02-17 16:14:25 +0300

    Added tests for the Product decrement functionality

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -22,6 +22,17 @@ describe('Product', () => {
     wrapper.find('[data-id="product-increment"]').simulate('click');
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
   });
+  it('should decrement amount', () => {
+    const wrapper = mount(<Product product={product} />);
+    wrapper.find('[data-id="product-increment"]').simulate('click').simulate('click');
+    wrapper.find('[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+  });
+  it('shouldn\'t decrement amount less than 0', () => {
+    const wrapper = mount(<Product product={product} />);
+    wrapper.find('[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
+  });
   it('should fetch data', () => {
     const fn = jest.fn();
     mount(<Product product={product} fetchData={fn} />);
