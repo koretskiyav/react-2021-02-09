a52d7521e02d389d476fd1df4d3a90e34283e284
AnnaKondr <anna.golov4enko@gmail.com>
2021-02-18 20:08:49 +0200

    HT2 - Kondratovets

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import Rate from '../../rate';
 import styles from './review.module.css';
 
-const Review = ({ user, text, rating }) => (
-  <div className={styles.review}>
+const Review = ({ id, user, text, rating }) => (
+  <div className={styles.review} data-name="review" data-id={`review-${id}`}>
     <div className={styles.content}>
       <div>
-        <h4 className={styles.name}>{user}</h4>
-        <p className={styles.comment}>{text}</p>
+        <h4 className={styles.name} data-id={`review-user`}>{user}</h4>
+        <p className={styles.comment} data-id="review-text">{text}</p>
       </div>
       <div className={styles.rate}>
         <Rate value={rating} />
@@ -19,6 +19,13 @@ const Review = ({ user, text, rating }) => (
 
 Review.defaultProps = {
   user: 'Anonymous',
+};
+
+Review.propTypes = {
+  id: PropTypes.string,
+  user: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  rating: PropTypes.number,
 };
 
 export default Review;
