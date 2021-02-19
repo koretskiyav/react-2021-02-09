b3b521e63d0d3a914e67e208e20af30f4bb7c591
elveret <lena.veret@gmail.com>
2021-02-19 16:03:41 +0300

    Change renedring

--- src/components/reviews/reviews.test.js
+++ src/components/reviews/reviews.test.js
@@ -1,4 +1,4 @@
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import Reviews from './reviews';
@@ -9,7 +9,7 @@ const rev = restaurants[0].reviews;
 Enzyme.configure({ adapter: new Adapter() });
 describe('Reviews', () => {
   it('should render', () => {
-    const wrapper = shallow(<Reviews key={rev[0].id} reviews={rev} />);
+    const wrapper = mount(<Reviews key={rev[0].id} reviews={rev} />);
     expect(wrapper.find('[data-id="reviews-test"]').length).toBe(1);
   });
 });
