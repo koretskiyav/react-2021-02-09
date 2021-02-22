88187fb06aae7b39c057a746a9f673a476764e3c
Comrade-Ivan <1335308@gmail.com>
2021-02-23 01:29:05 +0300

    added Basket

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,30 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Restaurants from '../restaurants';
 import Header from '../header';
+import Basket from '../basket';
+
+import styles from './app.module.css';
 
 export default class App extends PureComponent {
+  state = {
+    basketOpened: false,
+  }
+
+  openBasket = () => {
+    this.setState({basketOpened: true});  
+  }   
+  closeBasket = () => {
+    this.setState({basketOpened: false});  
+  } 
+
   render() {
     return (
-      <div>
-        <Header />
-        <Restaurants restaurants={this.props.restaurants} />
+      <div className={styles.app}>
+        <div className={styles.main}>
+          <Header openBasket={this.openBasket}/>
+          <Restaurants restaurants={this.props.restaurants} />
+        </div>
+        <Basket closeBasket={this.closeBasket} opened={this.state.basketOpened}/>
       </div>
     );
   }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { ReactComponent as Logo } from '../../icons/logo.svg';
 import styles from './header.module.css';
 
-const Header = () => (
+const Header = ({openBasket}) => (
   <header className={styles.header}>
+    <div></div>
     <Logo />
+    <button onClick={openBasket} className={styles.cartButton}>&#128722;</button>
   </header>
 );
 
