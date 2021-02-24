6a71414fbb07d2e5ef34259d5f29dab61538258b
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-02-24 20:52:34 +0100

    Save restaurant, review, user as key => value

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,27 +1,28 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
 import Rate from '../rate';
 import Tabs from '../tabs';
 
-const Restaurant = ({ restaurant }) => {
-  const { name, menu, reviews } = restaurant;
-
-  const averageRating = useMemo(() => {
-    const total = reviews.reduce((acc, { rating }) => acc + rating, 0);
-    return Math.round(total / reviews.length);
-  }, [reviews]);
+const Restaurant = ({ id, restaurant }) => {
+  const averageRating = 0;
+  // useMemo(() => {
+  //   const total = reviews.reduce((acc, { rating }) => acc + rating, 0);
+  //   return Math.round(total / reviews.length);
+  // }, [reviews]);
 
   const tabs = [
-    { title: 'Menu', content: <Menu menu={menu} /> },
-    { title: 'Reviews', content: <Reviews reviews={reviews} /> },
+    { title: 'Menu', content: <Menu menu={restaurant.menu} /> },
+    { title: 'Reviews', content: <Reviews reviews={restaurant.reviews} /> },
   ];
 
   return (
     <div>
-      <Banner heading={name}>
+      <Banner heading={restaurant.name}>
         <Rate value={averageRating} />
       </Banner>
       <Tabs tabs={tabs} />
@@ -40,5 +41,7 @@ Restaurant.propTypes = {
     ).isRequired,
   }).isRequired,
 };
-
-export default Restaurant;
+export default connect((state, props) => {
+  const restaurant = state.restaurants[props.id];
+  return { restaurant };
+})(Restaurant);
