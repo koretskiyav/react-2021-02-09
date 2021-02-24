a531476f94a4e6e68acdae4654e5a31c65c72bff
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-02-24 16:40:20 +0100

    Review from redux-store

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Rate from '../../rate';
 import styles from './review.module.css';
+import reviews from '../../../redux/reducer/reviews';
 
-const Review = ({ user, text, rating }) => (
+const Review = ({ id, review, user }) => (
   <div className={styles.review} data-id="review">
     <div className={styles.content}>
       <div>
         <h4 className={styles.name} data-id="review-user">
-          {user}
+          {user.name}
         </h4>
         <p className={styles.comment} data-id="review-text">
-          {text}
+          {review.text}
         </p>
       </div>
       <div className={styles.rate}>
-        <Rate value={rating} />
+        <Rate value={review.rating} />
       </div>
     </div>
   </div>
@@ -32,4 +34,10 @@ Review.defaultProps = {
   user: 'Anonymous',
 };
 
-export default Review;
+const mapStateToProps = (state, props) => {
+  const review = state.reviews.find((review) => review.id === props.id);
+  const user = state.users.find((user) => user.id === review.userId);
+  return { review, user };
+};
+
+export default connect(mapStateToProps)(Review);
