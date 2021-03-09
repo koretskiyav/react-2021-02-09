0815cf1614a49df957ad4d2b9a854e456f7474b6
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-03-09 11:38:23 +0100

    Add toggle of currency

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,23 +5,27 @@ import Basket from '../basket';
 
 import RestaurantsPage from '../../pages/restaurants-page';
 import { UserProvider } from '../../contexts/user-context';
+import { CurrencyProvider } from '../../contexts/currency-context';
 
-const App = (restaurants) => {
+const App = () => {
   const [name, setName] = useState('Igor');
+  const [currentCurrency, setCurrentCurrency] = useState('$');
 
   return (
     <div>
-      <UserProvider value={{ name, setName }}>
-        <Header />
-        <Switch>
-          <Route path="/checkout" component={Basket} />
-          <Route path="/restaurants" component={RestaurantsPage} />
-          <Route path="/error" component={() => <h1>Error Page!</h1>} />
-          <Route path="/">
-            <Redirect to="/restaurants" />
-          </Route>
-        </Switch>
-      </UserProvider>
+      <CurrencyProvider value={{ currentCurrency, setCurrentCurrency }}>
+        <UserProvider value={{ name, setName }}>
+          <Header />
+          <Switch>
+            <Route path="/checkout" component={Basket} />
+            <Route path="/restaurants" component={RestaurantsPage} />
+            <Route path="/error" component={() => <h1>Error Page!</h1>} />
+            <Route path="/">
+              <Redirect to="/restaurants" />
+            </Route>
+          </Switch>
+        </UserProvider>
+      </CurrencyProvider>
     </div>
   );
 };
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,30 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { userContext } from '../../contexts/user-context';
+import { currencyContext } from '../../contexts/currency-context';
 
 import { ReactComponent as Logo } from '../../icons/logo.svg';
 import styles from './header.module.css';
 
 const Header = () => {
   const { name, setName } = useContext(userContext);
+  const { currentCurrency, setCurrentCurrency } = useContext(currencyContext);
 
   return (
     <header className={styles.header} onClick={() => setName('Ivan')}>
+      <div className={styles.selectCur}>
+        <label htmlFor="currency">Choose a currency:</label>
+        <select
+          name="currency"
+          id="currency"
+          value={currentCurrency}
+          onChange={(e) => setCurrentCurrency(e.target.value)}
+        >
+          <option value="$">Dollar</option>
+          <option value="€">Euro</option>
+          <option value="₽">Ruble</option>
+        </select>
+      </div>
       <Link to="/restaurants">
         <Logo />
       </Link>
